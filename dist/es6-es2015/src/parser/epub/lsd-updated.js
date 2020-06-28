6f8c8b28f04bb2a7b898dc497044405152a7af86
Daniel Weck <daniel.weck@gmail.com>
2020-06-28 23:18:47 +0100

    add new dist

--- dist/es6-es2015/src/parser/epub/lsd-updated.js
+++ dist/es6-es2015/src/parser/epub/lsd-updated.js
@@ -3,21 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Updated = void 0;
 const tslib_1 = require("tslib");
 const ta_json_x_1 = require("ta-json-x");
-let Updated = (() => {
-    let Updated = class Updated {
-    };
-    tslib_1.__decorate([
-        ta_json_x_1.JsonProperty("license"),
-        tslib_1.__metadata("design:type", Date)
-    ], Updated.prototype, "License", void 0);
-    tslib_1.__decorate([
-        ta_json_x_1.JsonProperty("status"),
-        tslib_1.__metadata("design:type", Date)
-    ], Updated.prototype, "Status", void 0);
-    Updated = tslib_1.__decorate([
-        ta_json_x_1.JsonObject()
-    ], Updated);
-    return Updated;
-})();
+let Updated = class Updated {
+};
+tslib_1.__decorate([
+    ta_json_x_1.JsonProperty("license"),
+    tslib_1.__metadata("design:type", Date)
+], Updated.prototype, "License", void 0);
+tslib_1.__decorate([
+    ta_json_x_1.JsonProperty("status"),
+    tslib_1.__metadata("design:type", Date)
+], Updated.prototype, "Status", void 0);
+Updated = tslib_1.__decorate([
+    ta_json_x_1.JsonObject()
+], Updated);
 exports.Updated = Updated;
 //# sourceMappingURL=lsd-updated.js.map
--- dist/es7-es2016/src/parser/epub/lcp-userkey.js
+++ dist/es7-es2016/src/parser/epub/lcp-userkey.js
@@ -3,25 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UserKey = void 0;
 const tslib_1 = require("tslib");
 const ta_json_x_1 = require("ta-json-x");
-let UserKey = (() => {
-    let UserKey = class UserKey {
-    };
-    tslib_1.__decorate([
-        ta_json_x_1.JsonProperty("text_hint"),
-        tslib_1.__metadata("design:type", String)
-    ], UserKey.prototype, "TextHint", void 0);
-    tslib_1.__decorate([
-        ta_json_x_1.JsonProperty("algorithm"),
-        tslib_1.__metadata("design:type", String)
-    ], UserKey.prototype, "Algorithm", void 0);
-    tslib_1.__decorate([
-        ta_json_x_1.JsonProperty("key_check"),
-        tslib_1.__metadata("design:type", String)
-    ], UserKey.prototype, "KeyCheck", void 0);
-    UserKey = tslib_1.__decorate([
-        ta_json_x_1.JsonObject()
-    ], UserKey);
-    return UserKey;
-})();
+let UserKey = class UserKey {
+};
+tslib_1.__decorate([
+    ta_json_x_1.JsonProperty("text_hint"),
+    tslib_1.__metadata("design:type", String)
+], UserKey.prototype, "TextHint", void 0);
+tslib_1.__decorate([
+    ta_json_x_1.JsonProperty("algorithm"),
+    tslib_1.__metadata("design:type", String)
+], UserKey.prototype, "Algorithm", void 0);
+tslib_1.__decorate([
+    ta_json_x_1.JsonProperty("key_check"),
+    tslib_1.__metadata("design:type", String)
+], UserKey.prototype, "KeyCheck", void 0);
+UserKey = tslib_1.__decorate([
+    ta_json_x_1.JsonObject()
+], UserKey);
 exports.UserKey = UserKey;
 //# sourceMappingURL=lcp-userkey.js.map
